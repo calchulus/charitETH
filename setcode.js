15e5f494915c3f5625eb4084a6f342a18d3a3244
Calvin Chu <calc@uchicago.edu>
2018-12-09 01:33:26 +0800

    Add files via upload

---
+++ setcode.js
@@ -0,0 +1,50 @@
+
+
+
+/**
+ * Approve all component tokens for transfer to the Set Protocol contracts
+ */
+const approveTokensForTransfer = (tokenAddresses) => {
+  tokenAddresses.forEach(async function(address) {
+    await setProtocol.setUnlimitedTransferProxyAllowanceAsync(
+      address, { gas: 30000, gasPrice: 6000000000 },
+    );
+  });
+};
+
+//Adding some ropsten addresses	
+	
+const trueUSDAddress = '0x02Ca5A9c33585C06336481559FB0eadd3d656324';
+const daiAddress = '0xb224a70ef6ee268cb0dadb6c06a95e3b8040793d';
+
+approveTokensForTransfer([trueUSDAddress, daiAddress]);
+	
+	// Grab the setAddress after creating our Set
+const stableSetAddress = createStableSet();
+
+const issueStableSet = async function() {
+  // Issue 1x StableSet which equals 10 ** 18 base units.
+  const issueQuantity = new BigNumber(10 ** 18);
+
+  // Check that our issue quantity is divisible by the natural unit.
+  const isMultipleOfNaturalUnit = await setProtocol.setToken.isMultipleOfNaturalUnitAsync(stableSetAddress, issueQuantity);
+
+  if (isMultipleOfNaturalUnit) {
+    try {
+      return await setProtocol.issueAsync(
+        stableSetAddress,
+        issueQuantity,
+        {
+          from: '0xYourMetaMaskAddress',
+          gas: 4000000,
+          gasPrice: 8000000000,
+        },
+      );
+    } catch (err) {
+      throw new Error(`Error when issuing a new Set token: ${err}`);
+    }
+  }
+  throw new Error(`Issue quantity is not multiple of natural unit. Confirm that your issue quantity is divisible by the natural unit.`);
+};
+
+const issueTxHash = await issueStableSet();
